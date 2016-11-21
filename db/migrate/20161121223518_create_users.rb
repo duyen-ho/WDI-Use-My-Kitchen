193182172a2fd99f2e6940b32d630b5af05cc26a
PakkuDon <pakku.pd@gmail.com>
2016-11-22 09:39:16 +1100

    Add user model
    - Add bcrypt and pry-rails
    - Add migration

---
+++ db/migrate/20161121223518_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.text :name
+      t.text :email
+      t.text :password_digest
+      t.text :photo_url
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
