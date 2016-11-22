9bcba291c8402cd9a0025fd7c576761317dc8cc4
Jason Pompon <jasonpompon@gmail.com>
2016-11-22 15:16:49 +1100

    add validations to users fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true
+  validates :name, length: { minimum: 2 }
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+  validates :password, length: { in: 6..20 }
+
+  # TODO *** reminder to re-ative before deploy ***
+  # validate :password_complexity
+  # def password_complexity
+  #    if password.present? and not password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d). /)
+  #      errors.add :password, "must include at least one lowercase letter, one uppercase letter, and one digit"
+  #    end
+  # end
+
+  validates :description, presence: true
+  validates :description, length: { minimum: 10 }
+
+  validates :photo_url, presence: true
+  validates :photo_url, length: { minimum: 2 }
+
 end
