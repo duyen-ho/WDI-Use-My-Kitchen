e86bb1e509ddd81d899b2ef8b0db16427e3a167f
Jason Pompon <jasonpompon@gmail.com>
2016-11-22 12:06:37 +1100

    login/session working

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   get '/users/new', to: 'api/users#new'
 
   post '/api/users', to: 'api/users#create'
+
+  get '/session/new', to: 'session#new'
+  post '/session', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
+
 end
