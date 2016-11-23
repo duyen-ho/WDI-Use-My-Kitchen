be5050d6f33a0009fd036d9953e2a55bf2e0665d
PakkuDon <pakku.pd@gmail.com>
2016-11-23 12:33:39 +1100

    Add booking request model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123001608) do
+ActiveRecord::Schema.define(version: 20161123011826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "booking_requests", force: :cascade do |t|
+    t.datetime "booking_date"
+    t.string   "status"
+    t.integer  "user_id"
+    t.integer  "kitchen_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["kitchen_id"], name: "index_booking_requests_on_kitchen_id", using: :btree
+    t.index ["user_id"], name: "index_booking_requests_on_user_id", using: :btree
+  end
 
   create_table "bookings", force: :cascade do |t|
     t.datetime "booking_date"
@@ -64,6 +75,8 @@ ActiveRecord::Schema.define(version: 20161123001608) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "booking_requests", "kitchens"
+  add_foreign_key "booking_requests", "users"
   add_foreign_key "bookings", "kitchens"
   add_foreign_key "bookings", "users"
   add_foreign_key "kitchen_photos", "kitchens"
