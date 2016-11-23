47ea290f622466859cc4bd936cbcb5f68cf93777
bruno <bruno.rampinelli.rota@gmail.com>
2016-11-23 11:20:07 +1100

    geocoding

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -1,3 +1,14 @@
 class Kitchen < ApplicationRecord
   belongs_to :user
+
+  geocoded_by :full_street_address
+  after_validation :geocode
+
+
+  def full_street_address
+    [address, suburb, postcode,state, country].compact.join(', ')
+  end
+
+
+
 end
